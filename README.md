715ec886b5d4dd14c55399b8613041b652255d6a
Arccotangent <git@arccotangent.net>
2016-10-21 21:26:16 -0400

    Add README.md Add copyright messages Add license (GNU GPL version 3)

---
+++ README.md
@@ -0,0 +1,32 @@
+# pacchat
+
+PacChat is a direct P2P chat application that will encrypt and sign all messages you send, and verify the authenticity of all messages you receive.
+
+This program is still in beta.
+
+## Pros and Cons
+
+### Pros
+
+* Direct communication to the recipient's IP address, no middleman
+* 100% private chats, messages are not logged or sent to any third party
+* Messages are encrypted with RSA-4096 and AES-128
+* Messages are signed with SHA-512 and RSA-4096, and message authenticity is verified on arrival
+
+### Cons
+
+* Port forwarding required on both ends, however this should be taken care of automatically by PacChat through UPNP
+* Port forwarding means that PacChat is working on any public, school, or workplace network is highly unlikely
+
+## Downloading & Building
+
+```
+git clone https://github.com/Arccotangent/pacchat.git
+cd pacchat
+```
+
+Build on Unix-like systems: `./gradlew build`
+
+Build on Windows: `gradlew build`
+
+To launch from source root: `java -jar build/libs/pacchat.jar`
--- src/main/java/net/arccotangent/pacchat/Main.java
+++ src/main/java/net/arccotangent/pacchat/Main.java
@@ -1,3 +1,21 @@
+/*
+PacChat - Direct P2P secure, encrypted private chats
+Copyright (C) 2016 Arccotangent
+
+PacChat is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses.
+*/
+
 package net.arccotangent.pacchat;
 
 import net.arccotangent.pacchat.crypto.MsgCrypto;
@@ -14,11 +32,28 @@ import java.util.Scanner;
 public class Main {
 	
 	private static Logger core_log = new Logger("CORE");
-	private static final String VERSION = "20161018";
+	private static final String VERSION = "20161021";
 	private static KeyPair keyPair;
 	private static final String ANSI_BOLD = "\u001B[1m";
 	private static final String ANSI_BLUE = "\u001B[34m";
 	private static final String ANSI_RESET = "\u001B[0m";
+	
+	private static void printCopyright() {
+		System.out.println("PacChat Copyright (C) 2016 Arccotangent");
+		System.out.println("This program comes with ABSOLUTELY NO WARRANTY");
+		System.out.println("This is free software, and you are welcome to redistribute it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.");
+	}
+	
+	private static void printFullCopyright() {
+		System.out.println("PacChat - Direct P2P secure, encrypted private chats");
+		System.out.println("Copyright (C) 2016 Arccotangent");
+		System.out.println();
+		System.out.println("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.");
+		System.out.println();
+		System.out.println("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.");
+		System.out.println();
+		System.out.println("You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses.");
+	}
 	
 	public static KeyPair getKeypair(){
 		return keyPair;
@@ -29,9 +64,12 @@ public class Main {
 		System.out.println(ANSI_BOLD + ANSI_BLUE + "exit - Exit chat mode and shut down PacChat." + ANSI_RESET);
 		System.out.println(ANSI_BOLD + ANSI_BLUE + "send/s <ip address> - Send a message. PacChat will prompt you to enter your message after you enter the command." + ANSI_RESET);
 		System.out.println(ANSI_BOLD + ANSI_BLUE + "reply/r - Reply to the last person to send you a message." + ANSI_RESET);
+		System.out.println(ANSI_BOLD + ANSI_BLUE + "copyright - Show the full copyright message");
 	}
 	
 	public static void main(String[] args) {
+		printCopyright();
+		System.out.println();
 		core_log.i("Initializing PacChat " + VERSION);
 		core_log.i("Creating installation if it doesn't already exist.");
 		KeyManager.createInstallationIfNotExist(); //This function handles everything from the installation to key gen
@@ -133,6 +171,9 @@ public class Main {
 					} else if (buf.equals(",")) {
 						core_log.i("Message cancelled.");
 					}
+					break;
+				case "copyright":
+					printFullCopyright();
 					break;
 				default:
 					core_log.e("Invalid chat command!");
