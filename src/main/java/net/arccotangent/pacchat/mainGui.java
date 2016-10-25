ddd7c44879ecb3a8a3ddee8f29787db6c90d806f
Franco Benner <francobenner@gmail.com>
2016-10-25 12:32:08 -0400

    mainGui creation Copyright window added

--- src/main/java/net/arccotangent/pacchat/mainGui.java
+++ src/main/java/net/arccotangent/pacchat/mainGui.java
@@ -27,6 +27,5 @@ public class mainGui extends JFrame {
     public void copyrightGui() {
         copyrightPanel.setText("PacChat - Direct P2P secure, encrypted private chats\nCopyright (C) 2016 Arccotangent\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses.");
     }
-
 }
 
