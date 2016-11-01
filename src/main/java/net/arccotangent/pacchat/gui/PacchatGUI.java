6cd22b9466e7533c98d5354ba2070dc5ee0ffc6a
Franco Benner <Francobenner@gmail.com>
2016-10-31 20:17:51 -0400

    Complete message send

--- src/main/java/net/arccotangent/pacchat/gui/PacchatGUI.java
+++ src/main/java/net/arccotangent/pacchat/gui/PacchatGUI.java
@@ -1,9 +1,27 @@
+/*
+This file is part of PacChat.
+
+PacChat is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PacChat is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PacChat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package net.arccotangent.pacchat.gui;
 
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import net.arccotangent.pacchat.Main;
 import net.arccotangent.pacchat.filesystem.KeyManager;
+import net.arccotangent.pacchat.net.Client;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.swing.*;
@@ -45,16 +63,19 @@ public class PacchatGUI extends JFrame {
 
     private void initMessages() {
 
+        // Receive Messages
+        // WIP
+
+
+        // Send Messages
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String ip = ipField.getText();
                 String msg = enteredText.getText();
-                Main.doCommand("send " + ip);
-                Main.doCommand(msg);
+                Client.sendMessage(msg, ip);
             }
         });
-
     }
 
     private void initServerManager() {
