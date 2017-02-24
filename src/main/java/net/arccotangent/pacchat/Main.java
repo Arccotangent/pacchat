45d98373c9a02da829fff7a48b70980d891da696
Franco Benner <Francobenner@gmail.com>
2017-02-24 10:09:45 -0500

    Update contributors

--- src/main/java/net/arccotangent/pacchat/Main.java
+++ src/main/java/net/arccotangent/pacchat/Main.java
@@ -92,6 +92,9 @@ public class Main {
 		System.out.println("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.");
 		System.out.println();
 		System.out.println("You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses.");
+		System.out.println();
+		System.out.println("Contributors: Franco Benner,");
+		System.out.println("              Arccotangent");
 	}
 	
 	public static KeyPair getKeypair(){
